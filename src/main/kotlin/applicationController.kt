33ccfb03b3e336db9c151eac4a7720cf64466951
Valyrn <valhernr@gmail.com>
2024-01-08 16:17:02 -0500

    i forgor

--- src/main/kotlin/applicationController.kt
+++ src/main/kotlin/applicationController.kt
@@ -1,15 +1,37 @@
 import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.layout.VBox
 import java.awt.Desktop
+import java.awt.Label
 import java.io.File
 import java.io.IOException
 
 class ApplicationController(){
-    fun initalize(){
-        println("Hello World")
+    val buttons: ArrayList<Button> = ArrayList()
+
+    @FXML
+    val pdf_buttons: VBox
+
+    @FXML
+    fun initialize(){
+        val pdfs = File("C:\\Users\\edenj\\IdeaProjects\\PDF_App_2\\src\\pdfs").walk().forEach {
+            if (it.isFile) {
+                buttons.add(
+                    Button("${it}")
+                )
+            }
+        }
+        println(buttons)
+//        print(pdfs)
     }
 
     @FXML
-    fun open_core_book(){
+    fun click_book(){
+        open_core_book(0)
+    }
+
+    @FXML
+    fun open_core_book(id: Int){
         var file = File("src/pdfs/Ars Magica 5E/City And Guild.pdf")
 
         try {
